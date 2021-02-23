73756a559f2db7a6d7a6556d2aa689345e4bcb71
tyfts1 <romannawaz01@gmail.com>
2021-02-23 12:14:23 +0200

    Fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CenzorComponent } from './cenzor/cenzor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CenzorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
